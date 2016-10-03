9ed38070aba79b14ca461c479ae509b8661070b6
渡辺りえ <watanabe.r@5sg.jp>
2016-10-03 16:25:01 +0900

    なんかちょっと違うけどcanvasの場合用

--- modules/mysqlConnection.js
+++ modules/mysqlConnection.js
@@ -1,4 +1,6 @@
 var mysql = require('mysql');
+var express = require('express');
+var app = express();
 
   /*
   *
@@ -8,23 +10,23 @@ var mysql = require('mysql');
 
 var connection;
 
-  /* local
-  var db_config = {
-    host     : 'localhost',
-    user     : 'root',
-    password : 'root',
-    database : 'chat_test_160801'
-  };
-  */
-
-  /* 本番 */
-  var db_config = {
-    host     : 'us-cdbr-iron-east-04.cleardb.net',
-    user     : 'b261359fb916b2',
-    password : '61ef1f35',
-    database : 'heroku_6722ee1e07d3f4d'
-  };
-
+  if(app.get('env') == 'development'){
+    //development
+    var db_config = {
+      host     : 'localhost',
+      user     : 'root',
+      password : 'root',
+      database : 'chat_test_160801'
+    };
+  }else{
+    /* 本番 */
+    var db_config = {
+      host     : 'us-cdbr-iron-east-04.cleardb.net',
+      user     : 'b261359fb916b2',
+      password : '61ef1f35',
+      database : 'heroku_6722ee1e07d3f4d'
+    };
+  }
 
 var pool  = mysql.createPool(db_config);
 
